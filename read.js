db32f36f4b3bd8fd73c8ba47706df07457fb9d2d
battsetsegbatbaatar <147494554+battsetsegbatbaatar@users.noreply.github.com>
2024-01-09 19:21:31 +0800

    jjfjf

--- read.js
+++ read.js
@@ -1,33 +1,86 @@
 const fs = require("node:fs");
+const http = require("http");
 
-fs.readFile("input.txt", "utf-8", function (err, data = 1) {
-  if (err) {
-    console.error(err);
-    return;
-  }
+const host = "localhost";
+const port = 8000;
 
-  console.log(`ta ${data} unshij baina`);
-  const context = String(Number(data) + 1);
-  fs.writeFile("input.txt", context, function (err) {
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end("My first server! Hi .... I'm BG");
+};
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
+http.createServer((req,res)=>{
+  const read = () => {
+    fs.readFile("input.json", "utf-8", function (err, data) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(JSON.parse(data));
+    });
+  };
+  read();
+  res.writeHead(200=>{
+    "Context-Type":"plain/html"
+})
+res.end
+})
+
+const read = () => {
+  fs.readFile("input.json", "utf-8", function (err, data) {
     if (err) {
       console.error(err);
       return;
     }
+    console.log(JSON.parse(data));
   });
-});
+};
+read();
 
-fs.readFile("input.txt", "utf-8", function (err, data) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  console.log(`ta ${data} unshij baina`);
-  const context = String(Number(data) + 2);
-  fs.writeFile("input.txt", context, function (err) {
+const create = (data) => {
+  fs.readFile("input.json", "utf-8", function (err, json) {
     if (err) {
       console.error(err);
       return;
     }
+    const previousData = JSON.parse(json);
+
+    previousData.push(data);
+    fs.writeFile("input.json", JSON.stringify(previousData), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
   });
+};
+create({
+  ip: "1234.22.11",
+  port: 5000,
+  id: 4,
+  name: "Battsetseg",
 });
+
+// const update = (id, data, option) => {
+//   fs.readFile("input.json", "utf-8", (err, json) => {
+//     if (err) return console.error(err);
+//     const previousData = JSON.parse(json);
+//     const dataToUpdate = previousData.find((element) => element.id === id);
+//     const newArray = previousData.filter((element) => element.id !== id);
+//     newArray.push(...dataToUpdate, ...data);
+//     fs.writeFile("input.json", JSON.stringify(newArray), (err) => {
+//       if (err) return console.error(err);
+//     });
+//   });
+// };
+// update(7, { name: "BG..." });
+
+// const delete =(id, data)=>{
+//   fs.readFile("input.json",'utf-8',(err,json)=>{
+//     if (err) return console.error(err);
+//     const previousData=J
+//   })
+// }
--- write.js
+++ write.js
@@ -1,19 +1,4 @@
-const fs = require("node:fs/promises");
 
-const writeText = "hey..";
-fs.writeFile("input.txt", writeText, (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(writeText);
-});
-const write = JSON.stringify({ ip: "1234.22.11", port: 3000 });
-console.log(JSON.parse(write));
-fs.writeFile("input.json", write, (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(write);
-});
+
+
+
