549e91fb5e06a4b8435550b173d2ad0f01f8842d
battsetsegbatbaatar <147494554+battsetsegbatbaatar@users.noreply.github.com>
2024-01-10 19:58:05 +0800

    done task

--- read.js
+++ read.js
@@ -5,64 +5,69 @@ const host = "localhost";
 const port = 8000;
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end("My first server! Hi .... I'm BG");
+  if (req.url === "/user") {
+    res.writeHead(200, ("Context-Type", "text/plain"));
+    res.end("Hi Finished!!!!!");
+  } else {
+    res.writeHead(500, ("Context-Type", "application/json"));
+    res.end(JSON.stringify(reading()));
+
+    // fs.readFile("input.json", "utf-8", function (err, data) {
+    //   if (err) {
+    //     console.error(err);
+    //     return;
+    //   }
+    //   const user = JSON.parse(data);
+    //   res.writeHead(500, ("Context-Type", "application/json"));
+    //   res.end(JSON.stringify(user));
+    // });
+  }
 };
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+  return console.log(`Server is running on http://${host}:${port}`);
 });
-http.createServer((req,res)=>{
-  const read = () => {
-    fs.readFile("input.json", "utf-8", function (err, data) {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(JSON.parse(data));
-    });
-  };
-  read();
-  res.writeHead(200=>{
-    "Context-Type":"plain/html"
-})
-res.end
-})
 
-const read = () => {
-  fs.readFile("input.json", "utf-8", function (err, data) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(JSON.parse(data));
-  });
+// const read = (res) => {
+//  fs.readFile("input.json", "utf-8", function (err, data) {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     const user = JSON.parse(data);
+//     res.writeHead(200, ("Context-Type", "application/json"));
+//     res.end(JSON.stringify(user));
+//   });
+// };
+
+const reading = () => {
+  const text = fs.readFileSync("index.json", "utf-8");
+  return JSON.parse(text);
 };
-read();
+// const listen = function (req, res) {};
+// const create = (data) => {
+//   fs.readFile("input.json", "utf-8", function (err, json) {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     const previousData = JSON.parse(json);
 
-const create = (data) => {
-  fs.readFile("input.json", "utf-8", function (err, json) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    const previousData = JSON.parse(json);
-
-    previousData.push(data);
-    fs.writeFile("input.json", JSON.stringify(previousData), (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-    });
-  });
-};
-create({
-  ip: "1234.22.11",
-  port: 5000,
-  id: 4,
-  name: "Battsetseg",
-});
+//     previousData.push(data);
+//     fs.writeFile("input.json", JSON.stringify(previousData), (err) => {
+//       if (err) {
+//         console.error(err);
+//         return;
+//       }
+//     });
+//   });
+// };
+// create({
+//   ip: "1234.22.11",
+//   port: 5000,
+//   id: 4,
+//   name: "Battsetseg",
+// });
 
 // const update = (id, data, option) => {
 //   fs.readFile("input.json", "utf-8", (err, json) => {
